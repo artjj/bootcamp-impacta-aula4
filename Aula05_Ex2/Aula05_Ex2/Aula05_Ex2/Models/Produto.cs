7999f20358bac4360ab2ccbdad1d9349c47aa372
artjj <62916393+artjj@users.noreply.github.com>
2021-09-13 22:29:07 -0300

    Leitor de csv

--- Aula05_Ex2/Aula05_Ex2/Aula05_Ex2/Models/Produto.cs
+++ Aula05_Ex2/Aula05_Ex2/Aula05_Ex2/Models/Produto.cs
@@ -6,5 +6,19 @@ namespace Aula05_Ex2
 {
     class Produto
     {
+        public string Nome { get; set; }
+
+        public DateTime DataDeValidade { get; set; }
+
+        public double Preco { get; set; }
+        public string Marca { get; set; }
+
+        public Produto(string nome, DateTime dataDeValidade, double preco, string marca)
+        {
+            Nome = nome;
+            DataDeValidade = dataDeValidade;
+            Preco = preco;
+            Marca = marca;
+        }
     }
 }
