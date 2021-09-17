745a4877278e51aa3d2d295a13823ada0c3754cd
artjj <62916393+artjj@users.noreply.github.com>
2021-09-17 00:45:17 -0300

    Listagem com LINQ

---
+++ Aula09/ListagemDeProdutos/ListagemDeProdutos/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListagemDeProdutos.Models
+{
+    class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataValidade { get; set; }
+        public double Valor { get; set; }
+    }
+}
