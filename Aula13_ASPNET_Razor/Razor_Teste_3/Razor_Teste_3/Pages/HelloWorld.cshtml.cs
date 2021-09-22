b385cfadc01db50177afa106c140aa25ba1f1925
artjj <62916393+artjj@users.noreply.github.com>
2021-09-22 17:01:44 -0300

    Criação projeto inicial Razor

---
+++ Aula13_ASPNET_Razor/Razor_Teste_3/Razor_Teste_3/Pages/HelloWorld.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Razor_Teste_3.Pages
+{
+    public class HelloWorldModel : PageModel
+    {
+        private readonly ILogger<HelloWorldModel> _logger;
+
+        public HelloWorldModel(ILogger<HelloWorldModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
