29f48311603073a7c9f8d2b3a2498c9f01c93b16
artjj <62916393+artjj@users.noreply.github.com>
2021-09-23 21:01:06 -0300

    CRUD com Scaffold

---
+++ Aula14_Teste_Scaffold/TesteScaffold/TesteScaffold/Pages/Movie/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TesteScaffold.Data;
+using TesteScaffold.Models;
+
+namespace TesteScaffold.Pages.Movie
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TesteScaffold.Data.TesteScaffoldContext _context;
+
+        public CreateModel(TesteScaffold.Data.TesteScaffoldContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public TesteScaffold.Models.Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
