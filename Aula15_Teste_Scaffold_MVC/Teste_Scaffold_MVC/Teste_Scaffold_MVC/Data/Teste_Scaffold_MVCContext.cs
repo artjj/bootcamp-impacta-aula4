8b765603083fb5a956fff1c32f785709b24d92c4
artjj <62916393+artjj@users.noreply.github.com>
2021-09-24 17:09:44 -0300

    Scaffold com MVC (Movies)

---
+++ Aula15_Teste_Scaffold_MVC/Teste_Scaffold_MVC/Teste_Scaffold_MVC/Data/Teste_Scaffold_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Teste_Scaffold_MVC.Models;
+
+namespace Teste_Scaffold_MVC.Data
+{
+    public class Teste_Scaffold_MVCContext : DbContext
+    {
+        public Teste_Scaffold_MVCContext (DbContextOptions<Teste_Scaffold_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Teste_Scaffold_MVC.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Aula15_Teste_Scaffold_MVC/Teste_Scaffold_MVC/Teste_Scaffold_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teste_Scaffold_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
