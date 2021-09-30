c5c4573709672100ab4b946d33f0b94136f96ca4
artjj <62916393+artjj@users.noreply.github.com>
2021-09-30 18:42:01 -0300

    Criação de API (TodoItem) com Entity e Scaffold

---
+++ Aula19_APIs/TodoAPI/TodoAPI/Models/TodoApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoApiDbContext : DbContext
+    {
+        public DbSet<TodoItem> todoItem { get; set; }
+        public TodoApiDbContext(DbContextOptions<TodoApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Aula19_APIs/TodoAPI/TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    [Table("TB_TODO_ITEM")]
+    public class TodoItem
+    {
+        [Key]
+        [Column("ID_TODO_ITEM")]
+        public int Id { get; set; }
+
+        [Column("NM_TODO_ITEM", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        [Column("IS_COMPLETED")]
+        public bool IsCompleted { get; set; }
+
+    }
+}
